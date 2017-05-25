e411d5e2ef02a2da8325e9a2cad7b56012cf5368
Igor Lopes <igor@nucleus.eti.br>
2017-05-25 17:18:49 -0300

    Send single sms

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 var http = require('http');
 
-module.exports = function() {
-
-    var zenvia = {};
+module.exports = function(zenvia) {
 
     var host = 'api-rest.zenvia360.com.br';
 
     var headers = {
-        "Authorization": "Basic " + new Buffer(process.env.zenvia.username + ":" + process.env.zenvia.password, "utf8").toString("base64"),
+        "Authorization": "Basic " + new Buffer(zenvia.username + ":" + zenvia.password, "utf8").toString("base64"),
         "Accept": "application/json",
         "Content-type": "application/json"
     };
 
-    var sendRequest = http.request({
-        host: host,
-        path: "/services/send-sms",
-        method: "POST",
-        headers: headers
-    }, function(response) {
-
-        var body = "";
-
-        response.on("data", function(chunk) {
-            body += chunk;
-        });
-
-        response.on("end", function() {
-            console.log(body);
-        });
-    });
-
-    zenvia.sendSms = function(body) {
-        sendRequest.write(JSON.stringify(body));
-        sendRequest.end();
+    return {
+        sendSms: function(body) {
+            return new Promise(function(resolve, reject) {
+                http.request({
+                    host: host,
+                    path: "/services/send-sms",
+                    method: "POST",
+                    headers: headers
+                }, function(response) {
+                    var body = '';
+                    response.on('data', function(d) {
+                        body += d;
+                    });
+                    response.on('end', function() {
+                        resolve(body);
+                    });
+                    response.on('error', function(err) {
+                        reject(err);
+                    });
+                }).end(JSON.stringify(body));
+            });
+        }
     };
-
-    return zenvia;
 };
